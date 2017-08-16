87ef12d10539f6367553478156812b144c635128
luna alison alvarez <cestlou@users.noreply.github.com>
2017-08-16 15:54:35 -0700

    messing around with children

---
+++ test.js
@@ -0,0 +1,24 @@
+function coolDogs(dog) {
+    
+}
+
+
+coolDogs(pancake)
+
+
+coolDogs(olive)
+
+
+
+
+ERROR var pancake is not defined
+
+
+var coolDog = "pancake";
+
+if (coolDog == "pancake") {
+    console.log("Pancake is cool");
+}
+else {
+    console.log("Someone else is cool");
+}
